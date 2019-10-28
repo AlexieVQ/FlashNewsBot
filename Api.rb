9fed968276f7039955c08fe6593aff07c01af56a
Alexie <alexie@3615yunohost.fr>
2019-10-28 22:59:35 +0100

    Classe Api

---
+++ Api.rb
@@ -0,0 +1,37 @@
+##
+# Type d'API.
+
+module ApiType
+	TWITTER = 1 		##< Twitter
+	# MASTODON = 2		##< Mastodon
+end
+
+##
+# Classe représentant l’API du réseau social utilisé (Twitter pour l'instant,
+# Mastodon à l’avenir), et les méthodes d'accès à cette API.
+
+class Api
+	
+	# @instance		=> Nom de domaine du réseau social (ex : twitter.com)
+	# @urlBase		=> URL de base de l'API (ex : https://api.twitter.com/)
+	
+	private_class_method :new
+	
+	##
+	# Connexion à l'API
+	
+	def Api.connecter(type, nomInstance = "twitter.com")
+		new(type, nomInstance)
+	end
+	
+	def initialize(type, nomInstance)
+		case type
+		when ApiType::TWITTER
+			@instance = "twitter.com"
+			@urlBase = "https://api.twitter.com/"
+		else
+			@instance = nomInstance
+			@urlBase = "https://" + @instance + "/"
+		end
+	end
+end
