b8ac34ee5b98eca1072374d5c20f5a55136acf42
Alexie <alexie@3615yunohost.fr>
2019-11-09 22:04:22 +0100

    classe Élement

---
+++ Element.rb
@@ -0,0 +1,19 @@
+##
+# Classe représentant un élément d'une information, qui peut être un personnage,
+# une situation, un lieu, une date, une déclaration...
+#
+# Un élément possède un poids, qui influence sur son choix aléatoire.
+
+class Element
+	
+	## Poids d'un élément, accessible en lecture.
+	attr :poids, false
+	
+	# La classe est une classe abstraite.
+	private_class_method :new
+	
+	def initialize(poids)
+		@poids = poids
+	end
+	
+end
