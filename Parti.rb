81ae0f5a61d2dc56ec84fe97150fe69a3983c719
Alexie <alexie@3615yunohost.fr>
2019-11-10 12:48:40 +0100

    Parti

---
+++ Parti.rb
@@ -0,0 +1,98 @@
+require_relative 'Element.rb'
+require_relative 'String.rb'
+
+##
+# Classe représentant une organisation politique.
+#
+# Une organisation politique est caractérisée par son identifiant, son nom, son
+# sigle, son type ('parti', 'syndicat', 'association'), son poids, ses adjectifs
+# masculin et féminin, sa localité.
+
+class Parti < Element
+	
+	# @nom		=> Nom du parti
+	# @sigle	=> Sigle du parti
+	# @adjm		=> Adjectif masculin
+	# @adjf		=> Adjectif féminin
+	
+	## Type ('parti', 'syndicat', 'association')
+	attr :type, false
+	## Localité
+	attr :localite, false
+	
+	##
+	# Pour créer une organisation politique, il faut son identifiant, son nom,
+	# son sigle, son type, son poids, ses adjectifs masculin et féminin, sa
+	# localité (classe Localite)
+	def Parti.creer(id, nom, sigle, type, poids, adjm, adjf, localite)
+		new(id, nom, sigle, type, poids, adjm, adjf, localite)
+	end
+	
+	## Méthode privée
+	def initialize(id, nom, sigle, type, poids, adjm, adjf, localite)
+		super(id, poids)
+		@nom = nom
+		@sigle = sigle
+		@type = type
+		@poids = poids
+		@adjm = adjm
+		@adjf = adjf
+		@localite = localite
+	end
+	
+	## Donne le nom du parti, avec l'article donné après l'avoir évalué
+	def nom(article)
+		return @nom.evaluer.modif_article(article)
+	end
+	
+	## Donne le sigle du parti, après l'avoir évalué
+	def sigle
+		return @sigle.evaluer
+	end
+	
+	## Donne l'adjectif masculin du parti, après l'avoir évalué
+	def adjm
+		return @adjm.evaluer
+	end
+	
+	## Donne l'adjectif féminin du parti, après l'avoir évalué
+	def adjf
+		return @adjf.evaluer
+	end
+	
+	## Donne le nom du parti
+	def to_s
+		return self.nom
+	end
+	
+	##
+	# Retourne l'attribut avec les paramètres demandés.  
+	# Attribut possibles :
+	# - "nom"
+	# - "sigle"
+	# - "adjm"
+	# - "adjf"
+	# - "localite"
+	# - "type"
+	# Paramètres possibles :
+	# - Article pour le nom
+	def retourner(attribut, parametres)
+		case attribut
+		when "nom"
+			return self.nom(parametres[0])
+		when "sigle"
+			return self.sigle
+		when "adjm"
+			return self.adjm
+		when "adjf"
+			return self.adjf
+		when "localite"
+			return @localite
+		when "type"
+			return @type
+		else
+			return self.to_s
+		end
+	end
+	
+end
