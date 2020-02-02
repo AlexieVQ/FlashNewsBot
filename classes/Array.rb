bbf0974992817911db2bd52ee0c97515bd43aa68
Alexie <alexie@3615yunohost.fr>
2020-02-02 13:16:26 +0100

    Documentation de Array

--- classes/Array.rb
+++ classes/Array.rb
@@ -2,22 +2,26 @@ require_relative 'String.rb'
 require_relative 'Erreurs.rb'
 
 ##
-# Ajout à la classe Array de méthodes pour obtenir un élément du tableau de
+# Ajout à la classe Array d'une méthode pour obtenir un élément du tableau de
 # manière aléatoire.
-#
-# Si les éléments ont un attribut poids, cela influencera sur le choix de
-# l'élément.
 
 class Array
 	
 	##
-	# Retourne un élément du tableau ou d'un tableau passé en paramètre
-	# aléatoirement.
+	# Retourne un élément du tableau aléatoirement
+	#
+	# Si les éléments ont un attribut +poids+, le choix est pondéré par ce
+	# poids. Sinon le poids est défini à +1+ par défaut.
+	#
+	# Paramètres :
+	# [+ajout+] Éléments à ajouter dans la recherche (Array, vide par défaut)
+	#
+	# Lève une *RuntimeError* si le tableau est vide.
 	def elt_alea(ajout = [])
 		somme_poids = 0
 		tab = self + ajout
-		if tab.length == 0 then
-			raise TableauVide, "Tableau vide"
+		if(tab.length == 0) then
+			raise "Tableau vide"
 		end
 		tab.each do |elt|
 			somme_poids += defined?(elt.poids) ? elt.poids : 1
@@ -26,7 +30,7 @@ class Array
 		nb_rand = rand(somme_poids)
 		tab.each do |elt|
 			nb_rand -= defined?(elt.poids) ? elt.poids : 1
-			if nb_rand <= 0 then
+			if(nb_rand <= 0) then
 				return elt
 			end
 		end
--- classes/Erreurs.rb
+++ classes/Erreurs.rb
@@ -3,9 +3,3 @@
 # existe déjà ou n'existe pas.
 
 class IndexErreur < StandardError; end
-
-##
-# Classe représentant une erreur qui apparaît lorsqu'il faut retourner un
-# élément aléatoire d'un tableau vide.
-
-class TableauVide < StandardError; end
