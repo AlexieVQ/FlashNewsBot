d7710276827f2555e9fabb7c4bdcd72bfb177470
Alexie <alexie@3615yunohost.fr>
2020-02-15 15:23:43 +0100

    Structure de la bdd

--- bdd/creation.sql
+++ bdd/creation.sql
@@ -1,184 +1,37 @@
 -- Commandes de création de la base de données
 
--- Table contenant les accroches
-CREATE TABLE accroche (
-	id SERIAL PRIMARY KEY,
-	accroche VARCHAR(280) NOT NULL,
-	poids INTEGER NOT NULL DEFAULT 10
+-- Table des Api
+CREATE TABLE apis (
+	id SERIAL PRIMARY KEY
 );
 
--- Type de localité
-CREATE TYPE type_localite AS ENUM ('ville', 'pays', 'region');
-
--- Localité (ville, pays, région)
-CREATE TABLE localite (
-	id SERIAL PRIMARY KEY,
-	nom VARCHAR(280) UNIQUE NOT NULL,
-	nom_en VARCHAR(280),
-	nom_colle VARCHAR(280),
-	poids INTEGER NOT NULL DEFAULT 10,
-	adjm VARCHAR(280),
-	adjf VARCHAR(280),
-	departement VARCHAR(280),
-	emoji VARCHAR(8),
-	type TYPE_LOCALITE NOT NULL
-);
-
--- Catégorie d'une personne et d'une information
-CREATE TABLE categorie (
-	id SERIAL PRIMARY KEY,
-	nom VARCHAR(32) UNIQUE NOT NULL
-);
-
--- Genre grammatical d'une personne
-CREATE TYPE genre AS ENUM ('M', 'F');
-
--- Personne
-CREATE TABLE pers (
-	id SERIAL PRIMARY KEY,
-	nom_colle VARCHAR(280) UNIQUE NOT NULL,
-	poids INTEGER NOT NULL DEFAULT 10,
-	genre GENRE NOT NULL,
-	id_cat INTEGER NOT NULL REFERENCES categorie(id),
-	id_pays INTEGER REFERENCES localite(id)
-);
-
--- Nom d'une personne
-CREATE TABLE nom_pers (
-	id SERIAL PRIMARY KEY,
-	nom VARCHAR(280) NOT NULL,
-	poids INTEGER NOT NULL DEFAULT 10,
-	id_pers INTEGER NOT NULL REFERENCES pers(id)
-);
-
--- Surnom d'une personne
-CREATE TABLE surnom (
-	id SERIAL PRIMARY KEY,
-	surnom VARCHAR(240) NOT NULL,
-	poids INTEGER NOT NULL DEFAULT 10,
-	id_pers INTEGER NOT NULL REFERENCES pers(id)
-);
-
-CREATE TYPE type_parti AS ENUM ('parti', 'syndicat', 'association');
-
--- Parti politique ou syndicat
-CREATE TABLE parti (
-	id SERIAL PRIMARY KEY,
-	nom VARCHAR(280) NOT NULL,
-	sigle VARCHAR(280) NOT NULL,
-	type TYPE_PARTI NOT NULL DEFAULT 'parti',
-	poids INTEGER NOT NULL DEFAULT 10,
-	adjm VARCHAR(280),
-	adjf VARCHAR(280),
-	id_pays INTEGER REFERENCES localite(id)
-);
-
--- Média (papier, TV, radio, internet…)
-CREATE TABLE media (
-	id SERIAL PRIMARY KEY,
-	nom VARCHAR(280) NOT NULL,
-	poids INTEGER NOT NULL DEFAULT 10
-);
-
--- Date d'un information
-CREATE TABLE date (
-	id SERIAL PRIMARY KEY,
-	date VARCHAR(280) NOT NULL,
-	poids INTEGER NOT NULL DEFAULT 10
-);
-
--- Lieu d'une information
-CREATE TABLE lieu (
-	id SERIAL PRIMARY KEY,
-	lieu VARCHAR(280) NOT NULL,
-	poids INTEGER NOT NULL DEFAULT 10
-);
-
--- Type d'une circonstance
-CREATE TYPE type_circo AS ENUM (
-	'specifique',	-- Circonstance spécifique à une information
-	'universel',	-- Circonstance utilisable dans tous les contextes
-	'accuse',		-- Quand le sujet accuse une personne
-	'est_accuse',	-- Quand le sujet est accusé par quelqu'un d'autre
-	'accusation'	-- Marche pour les deux types d'accusation
-);
-
--- Information
-CREATE TABLE info (
-	id SERIAL PRIMARY KEY,
-	type VARCHAR(280) NOT NULL,
-	description VARCHAR(280) UNIQUE,	-- Pour faciliter la lecture de la base,
-										-- non utilisé par le bot
-	type_circo TYPE_CIRCO NOT NULL,
-	hashtag VARCHAR(280),
-	poids INTEGER NOT NULL DEFAULT 10
-);
-
--- Catégories d'une information
-CREATE TABLE cat_info (
-	id_info INTEGER REFERENCES info(id),
-	id_cat INTEGER REFERENCES categorie(id),
-	PRIMARY KEY (id_info, id_cat)
-);
-
--- Action d'une information
-CREATE TABLE action (
-	id SERIAL PRIMARY KEY,
-	action TEXT NOT NULL,
-	poids INTEGER NOT NULL DEFAULT 10,
-	id_info INTEGER NOT NULL REFERENCES info(id)
-);
-
--- Circonstance d'une information
-CREATE TABLE circo (
-	id SERIAL PRIMARY KEY,
-	circo VARCHAR(280) NOT NULL,
-	poids INTEGER NOT NULL DEFAULT 10,
-	type TYPE_CIRCO NOT NULL,
-	id_info INTEGER REFERENCES info(id)
-);
-
--- Déclaration du sujet à propos de l'information
-CREATE TABLE decla (
-	id SERIAL PRIMARY KEY,
-	decla VARCHAR(280) NOT NULL,
-	poids INTEGER NOT NULL DEFAULT 10,
-	id_info INTEGER REFERENCES info(id),
-	id_pers INTEGER REFERENCES pers(id)
-);
-
--- Type du réseau social
-CREATE TYPE type_app AS ENUM ('twitter');
-
--- Application
-CREATE TABLE application (
-	id SERIAL PRIMARY KEY,
-	type TYPE_APP NOT NULL,
-	domaine VARCHAR(64) NOT NULL,
+-- Table des TwitterApi (spécialisation des Api)
+CREATE TABLE twitter_apis (
+	api_id INTEGER PRIMARY KEY REFERENCES apis(id),
 	username VARCHAR(64) NOT NULL,
-	api_key VARCHAR(256),
-	api_secret VARCHAR(256),
-	UNIQUE (domaine, username)
+	api_key VARCHAR(64),
+	api_secret VARCHAR(64)
 );
 
 -- Historique des status envoyés, avec leurs statistiques et les éléments
 -- utilisés
-CREATE TABLE historique (
+CREATE TABLE statuses (
 	id BIGINT NOT NULL,
-	id_app INTEGER NOT NULL REFERENCES application(id),
-	PRIMARY KEY (id, id_app),
+	api_id INTEGER NOT NULL REFERENCES apis(id),
+	PRIMARY KEY (id, api_id),
 	date TIMESTAMPTZ NOT NULL,
-	nb_partages INTEGER NOT NULL DEFAULT 0,
-	nb_likes INTEGER NOT NULL DEFAULT 0,
-	nb_reponses INTEGER NOT NULL DEFAULT 0,
-	id_info INTEGER NOT NULL REFERENCES info(id),
-	id_structure INTEGER NOT NULL REFERENCES structure(id)
+	id_info INTEGER NOT NULL,
+	partages INTEGER,
+	likes INTEGER,
+	reponses INTEGER,
+	citations INTEGER
 );
 
--- Personnes citées dans un status
+-- Historique des personnages envoyés dans les status
 CREATE TABLE historique_pers (
-	id_status BIGINT NOT NULL,
-	id_app INTEGER NOT NULL REFERENCES application(id),
-	id_pers INTEGER NOT NULL REFERENCES pers(id),
-	PRIMARY KEY (id_status, id_app, id_pers)
+	status_id INTEGER NOT NULL,
+	api_id INTEGER NOT NULL,
+	id_pers INTEGER NOT NULL,
+	FOREIGN KEY (status_id, api_id) REFERENCES statuses(id, api_id),
+	PRIMARY KEY (status_id, api_id, id_pers)
 );
