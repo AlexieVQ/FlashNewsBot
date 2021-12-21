b4aaed12d1a11e04118be1d823d15549c1891c5a
AlexieVQ <alexie@nephos.me>
2021-12-21 13:10:09 +0100

    Migration des médias

--- src/entites/media.rb
+++ src/entites/media.rb
@@ -1,13 +1,12 @@
 require "rosace"
 require_relative "acteur"
+require_relative "categories"
 
 class Media < Rosace::Entity
 
 	include Acteur
-	
-	self.file = "regles/media.csv"
 
-	TYPES = [:papier]
+	self.file = "regles/media.csv"
 
 	# @!attribute [r] value
 	#  @return [String]
@@ -26,11 +25,72 @@ class Media < Rosace::Entity
 
 	# @!attribute [r] types
 	#  @return [Array<Symbol>]
-	mult_enum :types, *TYPES
+	mult_enum :types, :papier, :tv, :radio, :internet
 
-	# @return [1, 2, 3]
-	def personne
-		3
+	mult_enum :categories, *CATEGORIES
+
+	# @!attribute [r] origine
+	#  @return [Lieu] Origine du média
+	reference :origine, :Lieu, :required
+
+	# @see #nom
+	def value
+		nom
 	end
-	
+
+	# @return [Integer] Poids du média dans les choix aléatoires
+	def weight
+		poids = super
+		# @type [Info, nil]
+		info = context.variable(:$info)
+		if info
+            if info.contient?(self)
+                return 1
+            end
+			cat_sujet = info.instance_variable_get(:@sujet).
+					respond_to?(:categories) ?
+					info.sujet.categories :
+					[]
+			((info.categories | cat_sujet) & categories).each { poids *= 20 }
+			if info.lieux.any? { |lieu| origine.parent?(lieu) }
+				poids *= 20
+			end
+		end
+		if Bot.compte
+			# if Bot.bdd.pers_recemment_poste(self, Bot.compte) > 0
+			# 	return 1
+			# end
+			# poids += Bot.bdd.interactions_pers(self, Bot.compte)
+			# @type [Integer]
+			taille = plain_value(:nom).length
+			poids += (taille - Bot.compte.tendances.
+					reduce(1000) do |distance, tendance|
+				[self.distance(tendance), distance].min
+			end) * 10
+		end
+		poids
+	end
+
+	# Retourne la distance avec la chaîne donnée
+	# @param chaine [String] chaîne à comparer
+	# @return [Integer] Distance entre les deux chaînes
+	def distance(chaine)
+        plain_value(:nom).levenshtein(chaine)
+	end
+
+	# @return [Array<String>] Catégories du média
+	def categories
+		super + [:media, :journalisme] + (types.include?(:tv) ? [:tv] : [])
+	end
+
+	# @return [1, 3] Personne du média (3e sauf pour FlashNewsBot à la 1e)
+	def personne
+		id == 27 ? 1 : 3
+	end
+
+	# @return [Integer] Entier supérieur à 1
+	def qte
+		50
+	end
+
 end
